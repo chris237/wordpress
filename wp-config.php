1a307d9c25d097be06d7075221ca8dfafb3efc61
Fabius <fablux217@gmail.com>
2024-07-29 20:54:34 +0100

    modif wpconfig

--- wp-config.php
+++ wp-config.php
@@ -25,7 +25,7 @@ define('WP_DEBUG', false );
 /** Nom de la base de données de WordPress. */
 define('DB_NAME', 'direx');
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'root');
+define('DB_USER', 'direx');
 /** Mot de passe de la base de données MySQL. */
 define('DB_PASSWORD', '');
 /** Adresse de l’hébergement MySQL. */
